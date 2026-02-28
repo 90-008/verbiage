6e236fa53a236da715380e12632333fad191ab15
Axi <98420477+axiand@users.noreply.github.com>
2026-03-01 00:16:22 +0100

    app: render text files

--- src/components/WikiPage.js
+++ src/components/WikiPage.js
@@ -8,32 +8,45 @@ module.exports.hydrate = ({
     currentWiki
 }) => {
     let isDirectory = currentFile.isDirectory
-    let document
-    if (!isDirectory) {
-        document = currentFile.read().content.toString('utf8')
-    } else {
-        document = currentFile.tryGetChild("readme.md", false)?.read().content.toString('utf8')
-    }
-
-    if (document == null) {
-        let newLink = currentDir.path == "/"
-            ? `/${currentWiki}/edit/~?name=README.md`
-            : `/${currentWiki}/edit/${currentDir.path}?name=README.md`
-        document = `*This directory has no readme file.* {{${newLink}|Click here to create one}}.`
-    }
-
-    let md = document
-    let mdSanitized = sanitizer.sanitize(md)
-    let ren = new Markdawn({ escaperFunction: sanitizer.escape }).render(mdSanitized)
 
     let presented = new FileListPresenter(currentDir).items()
 
+    let contentType = currentFile.mimeType
+    let isMediaFile = isDirectory == false && !contentType.startsWith("text/")
+
+    let markdown
+
+    if (!isMediaFile) {
+        let document
+        if (!isDirectory) {
+            document = currentFile.read().content.toString('utf8')
+        } else {
+            document = currentFile.tryGetChild("readme.md", false)?.read().content.toString('utf8')
+        }
+
+        if (document == null) {
+            let newLink = currentDir.path == "/"
+                ? `/${currentWiki}/edit/~?name=README.md`
+                : `/${currentWiki}/edit/${currentDir.path}?name=README.md`
+            document = `*This directory has no readme file.* {{${newLink}|Click here to create one}}.`
+        }
+
+        let dawn = new Markdawn({ escaperFunction: sanitizer.escape })
+
+        let textSanitized = sanitizer.sanitize(document)
+        markdown = contentType == "text/markdown" ? dawn.render(textSanitized) : dawn.renderPlainText(textSanitized)
+    } else {
+
+    }
+
+
     return {
-        markdown: ren,
+        markdown: markdown || {},
         fileList: presented,
         fileType: {
             document: !isDirectory,
-            directory: isDirectory
+            directory: isDirectory,
+            media: isMediaFile
         }
     }
 }
