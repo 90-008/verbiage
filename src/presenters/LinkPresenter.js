548a7d919a7361c0c52258b49ad6b95bd799aefe
Axi <98420477+axiand@users.noreply.github.com>
2026-03-24 04:24:48 +0100

    app: some commenting

--- src/presenters/LinkPresenter.js
+++ src/presenters/LinkPresenter.js
@@ -11,6 +11,7 @@ function linkPresenter(href) {
 
     if (href.match(LINK_REGEX)) return href
     if (href.startsWith("#")) return href
+    /* don't interfere with /+wiki-name/... */
     if (href.match(/\/?\+[a-z0-9]+.*/)) return href
 
     hrefMatch = href.match(/([^?#]+)([?#]?.+)?/)
@@ -19,16 +20,18 @@ function linkPresenter(href) {
 
     location = ""
 
-    if (hrefPath.startsWith("/")) {
+    if (hrefPath.startsWith("/")) { // /absolute/path -> /+wiki-name/w/absolute/path.md
         location = pathUtil.normalize(hrefPath)
-    } else if (hrefPath.startsWith("./")) {
+    } else if (hrefPath.startsWith("./")) { // ./relative/path -> /+wiki-name/w/subdir/relative/path.md
         location = pathUtil.join(currentDir.pathNormalized, hrefPath)
     } else {
         location = pathUtil.join(currentDir.pathNormalized, hrefPath)
     }
 
     let locationDug = currentDir.manager.dig(location)
+    /* if the location is not found, try again but with .md */
     if (!locationDug.file) locationDug = currentDir.manager.dig(location + ".md")
+    /* if it's still not found, just spit the href back out with the current wiki */
     if (!locationDug.file) return pathUtil.normalize(`/${currentWiki}/w/${location}`)
 
     href = `/${currentWiki}/w/${locationDug.file.pathNormalized}${hrefExtras}`
